3309de8f732d959ddc1df325b9b262f619fab096
Oluwafemi Aremu <aremufemmy02@gmail.com>
2019-01-19 08:46:33 -0500

    inital commit

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const path = require('path');
 const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
 
+// load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// DB COnfig
+const db = require('./config/database');
+
+// Passport Config
+require('./config/passport')(passport);
 
 //Map-global promise - get rid of warning
 mongoose.Promise = global.Promise;
@@ -13,7 +26,7 @@ mongoose.Promise = global.Promise;
 
 
 //connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev', {
+mongoose.connect(db.mongoURI, {
     useNewUrlParser:true
 })
 
@@ -21,9 +34,7 @@ mongoose.connect('mongodb://localhost/vidjot-dev', {
 .catch(err => console.log(err));
 
 
-// Load Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
+
 
 
 // Handlebars Middleware
@@ -36,10 +47,35 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Method Override Middleware
 app.use(methodOverride('_method'));
 
+// Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  }))
 
+  // Passport Middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(flash());
+
+
+//GLobal variables
+app.use(function(req, res, next){
+   res.locals.success_msg = req.flash('success_msg');
+   res.locals.error_msg = req.flash('error_msg');
+   res.locals.error = req.flash('error');
+   res.locals.user = req.user || null;
+   next();
+
+});
 // Index Route
 app.get('/', (req, res) =>{
     const title = 'Welcome';
@@ -53,97 +89,10 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-//Idea Index Page
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-    .sort({date:'desc'})
-    .then(ideas => {
-        res.render('ideas/index', {
-            ideas,
-        });
-    });
-})
-
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-
-});
-
-// Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-    .then(idea => {
-        res.render('ideas/edit', {
-            idea:idea
-        });
-    })
-});
-
-// Process Form
-app.post('/ideas/:id', (req, res) => {
-    //console.log(req.body);
-   // res.send('ok');
-   let errors =[];
-
-    if(!req.body.title){
-        errors.push({text:'Please add a title'});
-    }
-
-    if(!req.body.details){
-        errors.push({text:'Please add some details'});
-    }
-
-    if(errors.length > 0)
-    {
-    res.render('ideas/add',{
-        errors: errors,
-        title: req.body.title,
-        details: req.body.details
-    });
-
-    } else {
-    //res.send('passed');
-    const newUser = {
-
-        title:req.body.title,
-        details:req.body.details,
-        user:req.body.details
-    }
-    new Idea(newUser)
-        .save()
-        .then(idea =>{
-            res.redirect('/ideas');
-        })
-    }
-});
-
-    //Edit Form Process
-app.put('/ideas/:id', (req, res) => {
-    //res.send('PUT');
-    Idea.findOne({
-        _id:req.params.id
-    })
-    .then(idea => {
-        // new values
-        idea.title = req.body.title;
-        idea.details = req.body.details;
-
-        idea.save()
-        .then(idea => {
-            res.redirect('/ideas');
-        })
-    });
-});
-
-    // Delete Idea
-app.delete('/delete/:id', (req, res) => {
-    res.send('DELETE');
-});
-
-const port = 5000;
+// use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
+const port = process.env.PORT || 5000;
 
 app.listen(port, () =>{
     console.log(`sever running on ${port}`)
